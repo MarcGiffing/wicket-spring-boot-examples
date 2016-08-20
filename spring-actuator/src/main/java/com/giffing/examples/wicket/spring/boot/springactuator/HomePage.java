1a102294ca103a5a35d9b93fd28e7143c07256aa
marcgiffing <marcgiffing@web.de>
2016-08-20 11:26:01 +0200

    added spring boot actuator example

---
+++ spring-actuator/src/main/java/com/giffing/examples/wicket/spring/boot/springactuator/HomePage.java
@@ -0,0 +1,9 @@
+package com.giffing.examples.wicket.spring.boot.springactuator;
+
+import org.apache.wicket.markup.html.WebPage;
+
+import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
+
+@WicketHomePage
+public class HomePage extends WebPage {
+}
