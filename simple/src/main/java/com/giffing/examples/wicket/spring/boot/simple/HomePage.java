ec32985c4032dc09a2153047da93f8cdd36126f2
marcgiffing <marcgiffing@web.de>
2017-01-15 13:33:45 +0100

    updated to latest version

--- simple/src/main/java/com/giffing/examples/wicket/spring/boot/simple/HomePage.java
+++ simple/src/main/java/com/giffing/examples/wicket/spring/boot/simple/HomePage.java
@@ -1,9 +1,31 @@
 package com.giffing.examples.wicket.spring.boot.simple;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.link.Link;
 
 import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
 
 @WicketHomePage
 public class HomePage extends WebPage {
+	
+	public HomePage() {
+		Form form = new Form("form"){
+			@Override
+			protected void onSubmit() {
+					setResponsePage(SecondPage.class);
+			}
+		};
+		queue(form);
+		
+		
+		add(new Link<Void>("mylink") {
+
+					@Override
+					public void onClick() {
+						setResponsePage(SecondPage.class);
+					}
+		        	
+		        });
+	}
 }
