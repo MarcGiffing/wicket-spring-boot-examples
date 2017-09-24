7f14830cd48b3462efb6d77a9d9ecd394c42ebd3
marcgiffing <marcgiffing@web.de>
2017-09-24 21:25:12 +0200

    war example

--- simple-war/src/main/java/com/giffing/examples/wicket/spring/boot/simplewar/WicketApplication.java
+++ simple-war/src/main/java/com/giffing/examples/wicket/spring/boot/simplewar/WicketApplication.java
@@ -2,9 +2,10 @@ package com.giffing.examples.wicket.spring.boot.simplewar;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WicketApplication {
+public class WicketApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws Exception {
 		new SpringApplicationBuilder()
